629f03aa170e279f854cf1a17933381024f5caf6
alimnette <matthieu.dumenil@gmail.com>
2021-09-05 15:43:26 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
+echo "Reset Machine-ID"
+truncate -s 0 /etc/machine-id
+rm /var/lib/dbus/machine-id
+ln -s /etc/machine-id /var/lib/dbus/machine-id
 
+echo "Reset Cloud-Init"
+cloud-init clean -s -l
+
+# https://kb.vmware.com/s/article/59687
+touch /etc/cloud/cloud-init.disabled
